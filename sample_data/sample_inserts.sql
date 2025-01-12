6206feea1982354196ff604f86ae8578ae1c7d5c
R3tuxn <jonas.limpl@vol.at>
2025-01-13 00:20:59 +0100

    update sample data

--- sample_data/sample_inserts.sql
+++ sample_data/sample_inserts.sql
@@ -1,24 +1,31 @@
-INSERT INTO plotsystem_v2.build_team (build_team_id, name)
-    VALUES (1, 'Alps BTE');
+INSERT INTO plotsystem_v2.build_team (build_team_id, name, api_key, api_create_date)
+    VALUES (1, 'Alps BTE', '331f5acc-9ce7-4fee-aef8-d0c535251c69', CURRENT_TIMESTAMP()),
+           (2, 'BTE Germany', null, null);
 
 INSERT INTO plotsystem_v2.server (build_team_id, server_name)
-    VALUES (1, 'ALPS-1');
+    VALUES (1, 'ALPS-1'),
+           (2, 'GER-1');
 
 INSERT INTO plotsystem_v2.country (country_code, continent, material)
     VALUES ('AT', 'EU', 'SKELETON_SKULL'),
-           ('CH', 'EU', 'SKELETON_SKULL');
+           ('CH', 'EU', 'SKELETON_SKULL'),
+           ('LI', 'EU', 'SKELETON_SKULL'),
+           ('DE', 'EU', 'SKELETON_SKULL');
 
 INSERT INTO plotsystem_v2.city_project (city_project_id, country_code, server_name)
     VALUES ('VIENNA_SUBURBS', 'AT', 'ALPS-1'),
-           ('ZURICH_SUBURBS', 'CH', 'ALPS-1');
+           ('ZURICH_SUBURBS', 'CH', 'ALPS-1'),
+           ('MUNICH_SUBURBS', 'DE', 'GER-1');
 
 INSERT INTO plotsystem_v2.build_team_has_country (build_team_id, country_code)
     VALUES (1, 'AT'),
-           (1, 'CH');
+           (1, 'CH'),
+           (2, 'DE');
 
 INSERT INTO plotsystem_v2.build_team_has_city_project (build_team_id, city_project_id)
     VALUES (1, 'VIENNA_SUBURBS'),
-           (1, 'ZURICH_SUBURBS');
+           (1, 'ZURICH_SUBURBS'),
+           (2, 'MUNICH_SUBURBS');
 
 INSERT INTO plotsystem_v2.builder (uuid, name, plot_type)
     VALUES ('3b350308-d857-4ecc-8b71-c93a2cf3c87b', 'R3tuxn', 1);
