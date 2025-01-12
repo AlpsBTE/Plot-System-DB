b777be98f75114361b77bb30a719823bd3d9c89e
R3tuxn <jonas.limpl@vol.at>
2025-01-12 18:53:29 +0100

    add sample inserts

---
+++ sample_data/sample_inserts.sql
@@ -0,0 +1,27 @@
+INSERT INTO plotsystem_v2.build_team (build_team_id, name)
+    VALUES (1, 'Alps BTE');
+
+INSERT INTO plotsystem_v2.server (build_team_id, server_name)
+    VALUES (1, 'ALPS-1');
+
+INSERT INTO plotsystem_v2.country (country_code, continent, material)
+    VALUES ('AT', 'EU', 'SKELETON_SKULL'),
+           ('CH', 'EU', 'SKELETON_SKULL');
+
+INSERT INTO plotsystem_v2.city_project (city_project_id, country_code, server_name)
+    VALUES ('VIENNA_SUBURBS', 'AT', 'ALPS-1'),
+           ('ZURICH_SUBURBS', 'CH', 'ALPS-1');
+
+INSERT INTO plotsystem_v2.build_team_has_country (build_team_id, country_code)
+    VALUES (1, 'AT'),
+           (1, 'CH');
+
+INSERT INTO plotsystem_v2.build_team_has_city_project (build_team_id, city_project_id)
+    VALUES (1, 'VIENNA_SUBURBS'),
+           (1, 'ZURICH_SUBURBS');
+
+INSERT INTO plotsystem_v2.builder (uuid, name, plot_type)
+    VALUES ('3b350308-d857-4ecc-8b71-c93a2cf3c87b', 'R3tuxn', 1);
+
+INSERT INTO plotsystem_v2.build_team_has_reviewer (build_team_id, uuid)
+    VALUES (1, '3b350308-d857-4ecc-8b71-c93a2cf3c87b');
